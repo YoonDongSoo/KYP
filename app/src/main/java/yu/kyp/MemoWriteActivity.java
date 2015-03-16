6d21b39edd2b8f766de48655e9f959e4d7e59c62
Dongsoo <burin84@gmail.com>
2015-03-16 11:46:42 +0900

    BLE통신 붙이기 1) BlunoBasicDemo에서 BLE관련 class를 가져오기 2) BLE스캔 3) 통신내용을 Log.d 로 출력

--- app/src/main/java/yu/kyp/MemoWriteActivity.java
+++ app/src/main/java/yu/kyp/MemoWriteActivity.java
@@ -2,16 +2,59 @@ package yu.kyp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import yu.kyp.bluno.BlunoLibrary;
 
-public class MemoWriteActivity extends ActionBarActivity {
+
+public class MemoWriteActivity extends BlunoLibrary {
+
+    private static final String TAG = MemoWriteActivity.class.getSimpleName();
+
+    @Override
+    public void onConectionStateChange(connectionStateEnum theConnectionState) {//Once connection state changes, this function will be called
+        switch (theConnectionState) {											//Four connection state
+            case isConnected:
+                Log.d(TAG, "Connected");
+                //buttonScan.setText("Connected");
+                break;
+            case isConnecting:
+                Log.d(TAG, "Connecting");
+                //buttonScan.setText("Connecting");
+                break;
+            case isToScan:
+                Log.d(TAG, "Scan");
+                //buttonScan.setText("Scan");
+                break;
+            case isScanning:
+                Log.d(TAG, "Scanning");
+                //buttonScan.setText("Scanning");
+                break;
+            case isDisconnecting:
+                Log.d(TAG, "isDisconnecting");
+                //buttonScan.setText("isDisconnecting");
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onSerialReceived(String theString) {							//Once connection data received, this function will be called
+        Log.d(TAG,theString);
+        //serialReceivedText.append(theString);							//append the text into the EditText
+        //The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_write);
+        onCreateProcess();
+        serialBegin(115200);
     }
 
 
@@ -33,7 +76,14 @@ public class MemoWriteActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id==R.id.action_scan)
+        {
+            buttonScanOnClickProcess();
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
